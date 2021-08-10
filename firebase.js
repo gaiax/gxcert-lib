57ae4eea8a0673964c3220ee0990f3c88207e397
takuto <takuto.yoshikai@gmail.com>
2021-08-10 15:33:02 +0900

    add firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVg-6cdqXuAnS2gECbjHlbJd_Lt8qyQWw",
+  authDomain: "gxcert-21233.firebaseapp.com",
+  projectId: "gxcert-21233",
+  storageBucket: "gxcert-21233.appspot.com",
+  messagingSenderId: "500830367528",
+  appId: "1:500830367528:web:dfdb8fe5fa6b362d8faa1d",
+  measurementId: "G-ZGY6X71Q2R"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+module.exports = firebase;
