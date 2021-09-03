202f7126a785a440fdf441d9f76eb9b1f78dd9a4
lucky <takuto.yoshikai@gmail.com>
2021-09-03 16:26:11 +0900

    icon fix

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const GxCertClient = require("../index");
 const Web3 = require("web3");
 const web3 = new Web3("https://matic-mumbai.chainstacklabs.com");
-const client = new GxCertClient(web3, "0xddcFaf6fF9232058D1E2A759008cfdC2d057C6c2", "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert");
+const client = new GxCertClient(web3, "0x1B172AF93f43781041347B97d364400E768a228c", "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert");
 function generatePrivateKey() {
   const chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
   let key = "";
@@ -41,6 +41,7 @@ const validUserCertificate = {
 const validProfile = {
   name: "alice",
   email: "alice@example.com",
+  icon: "icon",
 }
 let validCertificateCid;
 let groupId;
@@ -71,6 +72,7 @@ describe("GxCertClient", () => {
       const profile = await client.getProfile(address);
       assert.equal(profile.name, validProfile.name);
       assert.equal(profile.email, validProfile.email);
+      assert.equal(profile.icon, validProfile.icon);
     });
   });
   describe("Group", async () => {
