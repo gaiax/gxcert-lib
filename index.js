fa5ae0b41a7e625694ac908c21e0d75eceac693a
lucky <takuto.yoshikai@gmail.com>
2021-09-16 13:34:30 +0900

    create user certs

--- index.js
+++ index.js
@@ -518,6 +518,34 @@ class GxCertClient {
     hexString = "0x" + hexString;
     return hexString;
   }
+  async signUserCertificates(certId, from, tos, accountToSign) {
+    let unsigned = this.uintToHexString(certId) + from.toLowerCase();
+    for (const to of tos) {
+      unsigned += to.toLowerCase();
+    }
+    const hash = web3.utils.soliditySha3({
+      type: "string",
+      value: unsigned,
+    });
+    let signature;
+    if (accountToSign.privateKey) {
+      signature = await this.web3.eth.accounts.sign(
+        hash,
+        accountToSign.privateKey,
+      ).signature;
+    } else if (accountToSign.address) {
+      signature = await this.web3.eth.personal.sign(
+        hash,
+        accountToSign.address,
+      );
+    } else {
+      throw new Error("It needs an account to sign");
+    }
+    return {
+      signature,
+      hash,
+    }
+  }
   async signUserCertificate(userCertificate, accountToSign) {
     let certId = this.uintToHexString(userCertificate.certId);
     const hash = web3.utils.soliditySha3({
